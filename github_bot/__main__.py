7156ce5953adb149e91bc5135d8bf8a922f4d6cf
Anton Nikolaev <regwebghost@yandex.ru>
2020-02-21 15:59:36 +0300

    Init commit

---
+++ github_bot/__main__.py
@@ -0,0 +1,9 @@
+from octomachinery.app.server.runner import run as run_app
+
+
+if __name__ == '__main__':
+    run_app(
+        name='pyconby-2020-manmolecular-bot',
+        version='1.0.0',
+        url='https://github.com/apps/pyconby-2020-manmolecular-bot',
+    )
